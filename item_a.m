b1745bcd8f2b3d5570676d2e24bb238b87661b09
CAUA NICOLETT DE OLIVEIRA <caua.nicolett08@Dc5700>
2024-06-26 12:16:25 -0300

    Limpando o código e adicionando uma interface mais acessível

--- item_a.m
+++ item_a.m
@@ -1,46 +1,46 @@
 function matriz = item_a(valoresm)
-% Verifica se as entradas são válidas
-  %--- o vetor valoresm deve ser 1x4
-if (size(valoresm)(1) != 1 || size(valoresm)(2) != 4)
-  error("O tamanho da entrada deve ser 1x4");
-  %--- m deve ser divisível por 10
-elseif (mod(valoresm, 10) != 0)
-  error("As entadas do vetor valoresm devem ser divisíveis por 10");
-endif
-% Define quantos valores diferentes há de m e n.
-quantidadem = size(valoresm)(2);
-quantidadeMatrizes = 80;
+  % Verifica se as entradas são válidas
+    %--- o vetor valoresm deve ser 1x4
+  if (size(valoresm)(1) != 1 || size(valoresm)(2) != 4)
+    error("O tamanho da entrada deve ser 1x4");
+    %--- m deve ser divisível por 10
+  elseif (mod(valoresm, 10) != 0)
+    error("As entadas do vetor valoresm devem ser divisíveis por 10");
+  endif
+  % Define quantos valores diferentes há de m e n.
+  quantidadem = size(valoresm)(2);
+  quantidadeMatrizes = 80;
 
-% Cria vetor de índices para valores de n.
-indicesn = zeros(1,quantidadeMatrizes);
-for i = 1:quantidadeMatrizes;
- indicesn(i) = randi([1,5]);
-endfor
-
-% Cria matrizes mxn.
-contador = 1;
-for k = 1:4
- m = valoresm(k);
- valoresn = [1, m/10, m/2, 9*m/10, (m-1)];
- for i = 1:20
-   n = valoresn(indicesn(contador));
-   % Usa o contador para gerar o nome da matriz
-   matrizNome = strcat('A', num2str(contador));
-   matrizNomeModificada = strcat('a', num2str(contador));
-   matriz.(matrizNome) = rand(m,n);
-   if (n != 1)
-     ultimaColuna = matriz.(matrizNome)(:,n);
-     ultimaColuna = (10^(-5))*ultimaColuna;
-     mediaColunas = zeros(m,1);
-     for j = 1:n-1
-       mediaColunas = mediaColunas + matriz.(matrizNome)(:,j);
-     endfor
-     mediaColunas = mediaColunas/(n-1);
-     ultimaColuna = ultimaColuna + mediaColunas;
-     matriz.(matrizNomeModificada) = matriz.(matrizNome);
-     matriz.(matrizNomeModificada)(:,n) = ultimaColuna;
-   endif
-   contador++;
+  % Cria vetor de índices para valores de n.
+  indicesn = zeros(1,quantidadeMatrizes);
+  for i = 1:quantidadeMatrizes;
+   indicesn(i) = randi([1,5]);
   endfor
-endfor
+
+  % Cria matrizes mxn.
+  contador = 1;
+  for k = 1:4
+   m = valoresm(k);
+   valoresn = [1, m/10, m/2, 9*m/10, (m-1)];
+   for i = 1:20
+     n = valoresn(indicesn(contador));
+     % Usa o contador para gerar o nome da matriz
+     matrizNome = strcat('A', num2str(contador));
+     matrizNomeModificada = strcat('a', num2str(contador));
+     matriz.(matrizNome) = rand(m,n);
+     if (n != 1)
+       ultimaColuna = matriz.(matrizNome)(:,n);
+       ultimaColuna = (10^(-5))*ultimaColuna;
+       mediaColunas = zeros(m,1);
+       for j = 1:n-1
+         mediaColunas = mediaColunas + matriz.(matrizNome)(:,j);
+       endfor
+       mediaColunas = mediaColunas/(n-1);
+       ultimaColuna = ultimaColuna + mediaColunas;
+       matriz.(matrizNomeModificada) = matriz.(matrizNome);
+       matriz.(matrizNomeModificada)(:,n) = ultimaColuna;
+     endif
+     contador++;
+    endfor
+  endfor
 endfunction
--- main.m
+++ main.m
@@ -5,58 +5,7 @@ format long;
 vetor_m = [10,100,200,300];
 matriz = item_a(vetor_m);
 
-% Definindo a matriz
-A = matriz.A80;
-
-% Definindo o tamanho n
-m = size(A)(1);
-
-% ITEM 2 -------------------------------------
-
-% Definindo um vetor b aleatório de tamanho n com entradas entre -1 e 1:
-b = 1 - 2*rand(m,1);
-
-A2 = A'*A;
-c = A'*b;
-
-% Usando LU
-[L, U, P] = lu(A2);
-X3 = P*c;
-X2 = resolver_triangular(L, X3, "inferior");
-X = resolver_triangular(U, X2, "superior");
-
-% Usando Cholesky
-G = chol(A2, "upper");
-Y2 = resolver_triangular(G', c, "inferior");
-Y = resolver_triangular(G, Y2, "superior");
-
-% Usando QR
-[Q, R] = qr(A);
-numero_colunas = size(R)(2);
-c_hat = (Q'*b)(1:numero_colunas); R_hat = R(1:numero_colunas,:);
-Z = resolver_triangular(R_hat, c_hat, "superior");
-
-% ITEM 3 -------------------------------------
-
-% Calculando resíduos usando norma 1
-norma_b = norma1(b);
-residuoX = norma1(A*X - b)/norma_b;
-residuoY = norma1(A*Y - b)/norma_b;
-residuoZ = norma1(A*Z - b)/norma_b;
-
-% A tabela comparativa é da forma
-% [rx-rx   rx-ry   rx-rz]
-% [ry-rx   ry-ry   ry-rz]
-% [rz-rx   rz-ry   rz-rz]
-% onde rx é residuoX, ry é resíduoY e rz é residuoZ.
-
-residuos = [residuoX, residuoY, residuoZ];
-tabela_comparativa = ones(3);
-tabela_comparativa(1,:) = residuoX*tabela_comparativa(1,:) - residuos;
-tabela_comparativa(2,:) = residuoY*tabela_comparativa(2,:) - residuos;
-tabela_comparativa(3,:) = residuoZ*tabela_comparativa(3,:) - residuos;
-
-% ITEM 4 -------------------------------------
+% INICIALIZANDO MATRIZES DO ITEM 4 -----------
 
 % Gerando as transpostas
 for i = 1:80
@@ -90,38 +39,92 @@ for i = 1:80
   vet.(associado) = matriz.(transposta)*vet.(vetor_x);
 endfor
 
-% Instruções:
-% vet.l1 é o vetor lambda* gerado para a matriz matriz.AT1;
-% vet.x1 é o produto da transposta de matriz.AT1 por lambda;
-% vet.b1 é o produto de matriz.AT1 por vet.x1;
-% vet.ll1 é o vetor lambda vindo do sistema ponto sela com matriz.AT1;
-% vet.s1 é a solução de norma mínima vinda do sistema ponto sela
-% com a matriz matriz.AT1.
-% Você pode usar números de 1 a 80.
+% INICIALIZANDO MATRIZES DO ITEM 4 (FIM) -----------
 
-% Agora basta resolver o problema de minimização
-% Não confundir lambda* gerado aleatoriamente com o
-% lambda do método de multiplicadores de Lagrange.
+% Dê um número entre 1 e 80
+%while true
+  
+  indice = input("Digite um inteiro de 1 a 80: ");
+  if (isscalar(indice) && isnumeric(indice)
+    && indice <= 80 && indice >= 1 && mod(indice, 1) == 0)
+    
+    % Definindo a matriz
+    A = matriz.(strcat('A', num2str(indice)));
 
-% Usando sistema de ponto sela (resolver A*A'*lambda = -b
-% usando Cholesky, porque esqueci como faz gradiente conjugado
-for i = 1:80
-  lambda_lagrange = strcat('ll', num2str(i));
-  vetor_b = strcat('b', num2str(i));
-  matriz_nome = strcat('A', num2str(i));
-  transposta = strcat('AT', num2str(i));
-  H = chol(matriz.(transposta)*(matriz.(matriz_nome)), "upper");
-  aux = resolver_triangular(H', -vet.(vetor_b), "inferior");
-  vet.(lambda_lagrange) = resolver_triangular(H, aux, "superior");
-endfor
+    % Definindo o tamanho n
+    m = size(A)(1);
 
-% Encontrando o valor mínimo de x
-for i = 1:80
-  xmin = strcat('s', num2str(i));
-  lambda_lagrange = strcat('ll', num2str(i));
-  matriz_nome = strcat('A', num2str(i));
-  vet.(xmin) = -(matriz.(matriz_nome)*vet.(lambda_lagrange));
-  vet.(xmin) = -(matriz.(matriz_nome)*vet.(lambda_lagrange));
-endfor
+    % ITEM 2 -------------------------------------
+    % Definindo um vetor b aleatório de tamanho n com entradas entre -1 e 1:
+    b = 1 - 2*rand(m,1);
 
-% Usando fatoração QR
+    A2 = A'*A;
+    c = A'*b;
+
+    % Usando LU
+    [L, U, P] = lu(A2);
+    X3 = P*c;
+    X2 = resolver_triangular(L, X3, "inferior");
+    X = resolver_triangular(U, X2, "superior");
+
+    % Usando Cholesky
+    G = chol(A2, "upper");
+    Y2 = resolver_triangular(G', c, "inferior");
+    Y = resolver_triangular(G, Y2, "superior");
+
+    % Usando QR
+    [Q, R] = qr(A);
+    numero_colunas = size(R)(2);
+    c_hat = (Q'*b)(1:numero_colunas); R_hat = R(1:numero_colunas,:);
+    Z = resolver_triangular(R_hat, c_hat, "superior");
+
+    % ITEM 3 -------------------------------------
+
+    % Calculando resíduos usando norma 1
+    norma_b = norma1(b);
+    residuoX = norma1(A*X - b)/norma_b;
+    residuoY = norma1(A*Y - b)/norma_b;
+    residuoZ = norma1(A*Z - b)/norma_b;
+
+    % A tabela comparativa é da forma
+    % [rx-rx   rx-ry   rx-rz]
+    % [ry-rx   ry-ry   ry-rz]
+    % [rz-rx   rz-ry   rz-rz]
+    % onde rx é residuoX, ry é resíduoY e rz é residuoZ.
+
+    residuos = [residuoX, residuoY, residuoZ];
+    tabela_comparativa = ones(3);
+    tabela_comparativa(1,:) = residuoX*tabela_comparativa(1,:) - residuos;
+    tabela_comparativa(2,:) = residuoY*tabela_comparativa(2,:) - residuos;
+    tabela_comparativa(3,:) = residuoZ*tabela_comparativa(3,:) - residuos;
+
+    % Atualizando A para a sua transposta
+    A = A';
+    % Atualizando a definição do vetor b
+    b = vet.(strcat('b', num2str(indice)));
+
+    % Instruções:
+    % vet.l1 é o vetor lambda* gerado para a matriz matriz.AT1;
+    % vet.x1 é o produto da transposta de matriz.AT1 por lambda;
+    % vet.b1 é o produto de matriz.AT1 por vet.x1;
+    % Você pode usar números de 1 a 80.
+
+    % Agora basta resolver o problema de minimização
+    % Não confundir lambda* gerado aleatoriamente com o
+    % lambda do método de multiplicadores de Lagrange.
+
+    % Usando sistema de ponto sela (resolver A*A'*lambda = -b
+    % usando Cholesky, porque esqueci como faz gradiente conjugado
+
+    G = chol(A*A', "upper");
+    aux = resolver_triangular(G', -b, "inferior");
+    lambda = resolver_triangular(G, aux, "superior");
+
+    x_min = -A'*lambda;
+
+    % Usando fatoração QR
+    escolha = A;
+  else
+  error("ERR0");
+  endif
+%endwhile
